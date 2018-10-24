a4ab03f082bd8604d3037017a6bd71969cadd433
Axel Tavormina <tavormina.axel@gmail.com>
2018-10-24 13:29:57 +0200

    feat(Main-Header): design mobile & desktop

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,16 +1,17 @@
 import React from 'react';
+import { Icon } from 'semantic-ui-react';
 import styles from './Header.module.scss';
 
 const Header = () => (
   <header className={styles.header}>
     <h1 className={styles.title}>Markdown Editor</h1>
-
     <a
-      href="https://www.github.com/codekameleon/markdown-editor"
+      className={styles.githubLink}
+      href="https://github.com/codeKameleon/markdown-editor"
       target="_blank"
       rel="noopener noreferrer"
     >
-      Github
+      <Icon name="github" size="large" />
     </a>
   </header>
 );
